eb2e345e91db4697988387b86f95fa99406003f2
lucasdunster <60508486+lucasluvia@users.noreply.github.com>
2022-03-13 16:45:53 -0400

    Camera Move for Touch Input
    Modified camera to move through touch inputs + small map camera tag change

--- Assets/[Scripts]/MoveCamera.cs
+++ Assets/[Scripts]/MoveCamera.cs
@@ -7,6 +7,7 @@ public class MoveCamera : MonoBehaviour
     Camera camera;
     private Vector3 dragOrigin;
     private Vector3 clickOrigin = Vector3.zero;
+    private Vector3 touchOrigin = Vector3.zero;
     private Vector3 basePos = Vector3.zero;
 
     public bool canDragCamera = true;
@@ -37,9 +38,28 @@ public class MoveCamera : MonoBehaviour
             }
 
             transform.position = new Vector3(basePos.x + ((clickOrigin.x - dragOrigin.x) * .01f), transform.position.y, basePos.z + ((clickOrigin.y - dragOrigin.y) * .01f));
+
+            if(Input.touchCount >= 1)
+            {
+                if (touchOrigin == Vector3.zero)
+                {
+                    touchOrigin = Input.GetTouch(0).position;
+                    basePos = transform.position;
+                }
+                dragOrigin = Input.GetTouch(0).position;
+            }
+
+            if (Input.touchCount < 1)
+            {
+                touchOrigin = Vector3.zero;
+                return;
+            }
+
+            transform.position = new Vector3(basePos.x + ((touchOrigin.x - dragOrigin.x) * .01f), transform.position.y, basePos.z + ((touchOrigin.y - dragOrigin.y) * .01f));
+
         }
 
-        
+
     }
 
 }
