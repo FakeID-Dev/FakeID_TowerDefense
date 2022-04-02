4e87b38514d8ed94a4c551f47d605b92b151d4ea
Eriki Enos <Erikjenos@hotmail.com>
2022-04-02 16:34:26 -0700

    new roads and UI fixes

--- Assets/[Scripts]/TileOptions.cs
+++ Assets/[Scripts]/TileOptions.cs
@@ -7,150 +7,36 @@ public class TileOptions : MonoBehaviour
 {
     public int posX;
     public int posY;
-    public bool canBuild = false; 
-    public GameObject Panel;
-    public GameObject hovered;
-    public GameObject road1;
-    public GameObject road2;
+    public bool up = false, down = false, left = false, right = false;
+
+    public bool canBuild = false;
     public bool isSelected = false;
-    private float angle;
-    private float distance;
-    public bool hover;
     public bool UIcover = false;
-    public GameObject gameManager; 
+
+    public GameObject gameManager;
+
+    public GameObject roadDown;
+    public GameObject roadStr;
+    public GameObject roadLeft;
+    public GameObject roadRight;
+    public GameObject roadDowLeft;
+    public GameObject roadDowRight;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.Find("GameManager");
-        hover = false;
     }
     // Update is called once per frame
     void Update()
     {
-        UIcover = EventSystem.current.IsPointerOverGameObject();
-
-        if (EventSystem.current.IsPointerOverGameObject())
-        {
-            isSelected = false;
-        }
-
-        if (isSelected)
-        {
-
-            RaycastHit hit;
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            
-            if (Physics.Raycast(ray, out hit))
-            {
-                Vector3 targetDir = hit.transform.position - transform.position;
-                angle = Vector3.Angle(targetDir, transform.forward);
-
-               
-
-                float distance = Vector3.Distance(hit.transform.position, transform.position);
-                GameObject temp;
-
-                Debug.Log(angle);  
-
-                if (angle > 160 && angle <= 181)
-                {
-                    temp = Instantiate(road1, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 0.2f, gameObject.transform.position.z), road1.transform.rotation);
-                    gameManager.GetComponent<Initialize>().tileList.Add(temp);
-                    gameManager.GetComponent<Initialize>().roadList.Add(temp);
-                    AddToMapArray();
-                    gameManager.GetComponent<Initialize>().RemoveTile(gameObject.transform.parent.gameObject);
-                    gameManager.GetComponent<Initialize>().checkRoadCorners();
-                    gameObject.GetComponentInParent<TileDestroy>().tileDestroy();
-                }
-
-                if (angle == 90)
-                {
-                    temp = Instantiate(road2, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 0.2f, gameObject.transform.position.z), road2.transform.rotation);
-                    gameManager.GetComponent<Initialize>().tileList.Add(temp);
-                    gameManager.GetComponent<Initialize>().roadList.Add(temp);
-                    AddToMapArray();
-                    gameManager.GetComponent<Initialize>().RemoveTile(gameObject.transform.parent.gameObject);
-                    gameManager.GetComponent<Initialize>().checkRoadCorners();
-                    gameObject.GetComponentInParent<TileDestroy>().tileDestroy();
-                }
-
-                if (angle >= 0 && angle <= 45 && distance > 0.5f)
-                {
-                    Debug.Log("WORKING ");
-
-                    temp = Instantiate(road1, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 0.2f, gameObject.transform.position.z), road1.transform.rotation);
-                    gameManager.GetComponent<Initialize>().tileList.Add(temp);
-                    gameManager.GetComponent<Initialize>().roadList.Add(temp);
-                    AddToMapArray();
-                    gameManager.GetComponent<Initialize>().RemoveTile(gameObject.transform.parent.gameObject);
-                    gameManager.GetComponent<Initialize>().checkRoadCorners();
-                    gameObject.GetComponentInParent<TileDestroy>().tileDestroy();
-
-                }
-
-            }
-
-           
-
-        }
-    }
-
-    public void OnMouseDown()
-    {
-        if (gameManager.GetComponent<Inventory>().holding == false)
-        {
-            if (hover)
-            {
-                isSelected = true;
-            }
-        }
-    }
-
-    public void OnMouseUp()
-    {
-        if (gameManager.GetComponent<Inventory>().holding == false)
-        {
-            if (isSelected)
-            {
-                //gameObject.transform.position = new Vector3(gameObject.transform.position.x, 0, gameObject.transform.position.y);
-            }
-            isSelected = false;
-        }
-    }
-
-
-    public void OnMouseEnter()
-    {
-        if(gameManager.GetComponent<Inventory>().holding == false && UIcover == false && canBuild == true)
-        {
-            if (isSelected == false)
-            {
-                gameObject.transform.position += new Vector3(0, 0.2f, 0);
-                hover = true;
-            }
-        }
 
     }
 
-    public void OnMouseExit()
-    {
-        if (gameManager.GetComponent<Inventory>().holding == false)
-        {
-            if (isSelected == false)
-            {
-                Debug.Log("EXIT");
-                Vector3 temp1 = transform.position;
-                temp1.y = 0.0f;
-                transform.position = temp1;
-                hover = false;
-            }
-        }
-    }
 
-
-    void AddToMapArray()
+    public void AddToMapArray()
     {
         isSelected = false;
         for (int x = 0; x < 50; x++)
@@ -160,7 +46,47 @@ public class TileOptions : MonoBehaviour
                 gameManager.GetComponent<Initialize>().Map[posX, posY] = 1;
             }
         }
-        gameManager.GetComponent<Initialize>().UpdateRoad(posX, posY);
+    }
+
+    public void RoadType()
+    {
+        if (up || down)
+        {
+            if (left == false && right == false)
+            {
+                Instantiate(roadDown, new Vector3(transform.position.x, transform.position.y, transform.position.z), roadDown.transform.rotation);
+            }
+        }
+
+        if (left || right)
+        {
+            if (up == false && down == false)
+            {
+                Instantiate(roadStr, new Vector3(transform.position.x, transform.position.y, transform.position.z), roadStr.transform.rotation);
+            }
+        }
+
+        if (left && up)
+        {
+            Instantiate(roadLeft, new Vector3(transform.position.x, transform.position.y, transform.position.z), roadLeft.transform.rotation);
+        }
+
+        if (right && up)
+        {
+            Instantiate(roadRight, new Vector3(transform.position.x, transform.position.y, transform.position.z), roadRight.transform.rotation);
+        }
+
+        if (right && down)
+        {
+            Instantiate(roadDowLeft, new Vector3(transform.position.x, transform.position.y, transform.position.z), roadDowLeft.transform.rotation);
+        }
+
+        if (left && down)
+        {
+            Instantiate(roadDowRight, new Vector3(transform.position.x, transform.position.y, transform.position.z), roadDowRight.transform.rotation); ;
+        }
+
+
     }
 
 }
