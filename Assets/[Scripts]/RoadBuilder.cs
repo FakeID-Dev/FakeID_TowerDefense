48ca528ee87359a6786ae1627fb9dac13923374a
Eriki Enos <Erikjenos@hotmail.com>
2022-04-17 12:11:27 -0700

    Roads

--- Assets/[Scripts]/RoadBuilder.cs
+++ Assets/[Scripts]/RoadBuilder.cs
@@ -17,6 +17,7 @@ public class RoadBuilder : MonoBehaviour
     void Start()
     {
         gameManager = gameObject;
+        GetTileList();
     }
 
     // Update is called once per frame
@@ -42,65 +43,83 @@ public class RoadBuilder : MonoBehaviour
             {
                 if (Physics.Raycast(ray, out hit))
                 {
-                    startObj = hit.transform.gameObject;
-                    newRoadList.Add(hit.transform.gameObject);
+                    if (hit.transform.gameObject.tag == "Road" || hit.transform.gameObject.tag == "tile")
+                    {
+                        startObj = hit.transform.gameObject;
+                        newRoadList.Add(hit.transform.gameObject);
+                        gameManager.GetComponent<Initialize>().tileList.Add(hit.transform.gameObject);
+
+                    }
                 }
             }
             if (touch.phase == TouchPhase.Moved)
             {
                 if (Physics.Raycast(ray, out hit))
                 {
-                    hit.transform.gameObject.GetComponent<Renderer>().materials[1].SetColor("_Color", Color.green);
-
-                    if (newRoadList[newRoadList.Count - 1] != hit.transform.gameObject)
+                    if (hit.transform.gameObject.tag == "Road" || hit.transform.gameObject.tag == "tile")
                     {
-                        newRoadList.Add(hit.transform.gameObject);
-                    }
+                        hit.transform.gameObject.GetComponent<Renderer>().materials[1].SetColor("_Color", Color.green);
 
+                        if (newRoadList[newRoadList.Count - 1] != hit.transform.gameObject)
+                        {
+                            newRoadList.Add(hit.transform.gameObject);
+                            gameManager.GetComponent<Initialize>().tileList.Add(hit.transform.gameObject);
+                        }
+                    }
                 }
             }
 
             if (touch.phase == TouchPhase.Ended)
             {
-                
                 int x, y, size = newRoadList.Count;
                 GameObject temp;
                 bool up = false, down = false, left = false, right = false;
 
+                //GetTileList();
+
                 for (int c = 0; c < newRoadList.Count; c++)
                 {
-                    x = newRoadList[c].GetComponent<TileOptions>().posX;
-                    y = newRoadList[c].GetComponent<TileOptions>().posY;
+                    x = newRoadList[c].GetComponentInParent<TileOptions>().posX;
+                    y = newRoadList[c].GetComponentInParent<TileOptions>().posY;
 
                     for (int u = 0; u < newRoadList.Count; u++)
                     {
 
-                        if (x == newRoadList[u].GetComponent<TileOptions>().posX && y + 1 == newRoadList[u].GetComponent<TileOptions>().posY)
+                        if (x == newRoadList[u].GetComponentInParent<TileOptions>().posX && y + 1 == newRoadList[u].GetComponentInParent<TileOptions>().posY) 
                         {
-                            newRoadList[c].GetComponent<TileOptions>().up = true;
+                            newRoadList[c].GetComponentInParent<TileOptions>().up = true;
                             //Debug.Log("up");
                         }
 
-                        if (x + 1 == newRoadList[u].GetComponent<TileOptions>().posX && y == newRoadList[u].GetComponent<TileOptions>().posY)
+                        if (x + 1 == newRoadList[u].GetComponentInParent<TileOptions>().posX && y == newRoadList[u].GetComponentInParent<TileOptions>().posY)
                         {
-                            newRoadList[c].GetComponent<TileOptions>().right = true;
+                            newRoadList[c].GetComponentInParent<TileOptions>().right = true;
                             //Debug.Log("right");
                         }
 
-                        if (x == newRoadList[u].GetComponent<TileOptions>().posX && y - 1 == newRoadList[u].GetComponent<TileOptions>().posY)
+                        if (x == newRoadList[u].GetComponentInParent<TileOptions>().posX && y - 1 == newRoadList[u].GetComponentInParent<TileOptions>().posY)
                         {
-                            newRoadList[c].GetComponent<TileOptions>().down = true;
+                            newRoadList[c].GetComponentInParent<TileOptions>().down = true;
                             //Debug.Log("down");
                         }
 
-                        if (x - 1 == newRoadList[u].GetComponent<TileOptions>().posX && y == newRoadList[u].GetComponent<TileOptions>().posY)
+                        if (x - 1 == newRoadList[u].GetComponentInParent<TileOptions>().posX && y == newRoadList[u].GetComponentInParent<TileOptions>().posY)
                         {
-                            newRoadList[c].GetComponent<TileOptions>().left = true;
+                            newRoadList[c].GetComponentInParent<TileOptions>().left = true;
                             //Debug.Log("LEFT");
                         }
+
+                        
                     }
-                    newRoadList[c].GetComponent<TileOptions>().RoadType();
+                   
                 }
+                
+                for(int v = 0; v < newRoadList.Count;  v++)
+                {
+                    newRoadList[v].GetComponentInParent<TileOptions>().RoadType();
+                }
+                
+
 
                 if (size > 1)
                 {
@@ -156,5 +175,16 @@ public class RoadBuilder : MonoBehaviour
     public void OnCancelRoadSelect()
     {
         RoadSelect = false;
+    }
+
+    public void GetTileList()
+    {
+        for(int c = 0; c < gameManager.GetComponent<Initialize>().tileList.Count; c++)
+        {
+            if(gameManager.GetComponent<Initialize>().tileList[c].gameObject.tag == "Road")
+            {
+                newRoadList.Add(gameManager.GetComponent<Initialize>().tileList[c]);
+            } 
+        }
     }
 }
