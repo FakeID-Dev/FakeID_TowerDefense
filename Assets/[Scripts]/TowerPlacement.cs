29220d8fc91d05eda72d1dc294d79ad39d4dbe25
Sam Pollock <samcpollock@gmail.com>
2022-04-18 22:03:18 -0400

    Improvements to cannonball tower

--- Assets/[Scripts]/TowerPlacement.cs
+++ Assets/[Scripts]/TowerPlacement.cs
@@ -62,6 +62,7 @@ public class TowerPlacement : MonoBehaviour
 
         if (holding)
         {
+            
             if (canBePlaced)
             {
                 met[0].color = Color.green;
@@ -77,6 +78,7 @@ public class TowerPlacement : MonoBehaviour
         }
         else
         {
+           
             for (int x = 0; x < 3; x++)
             {
                 met[x].color = originalMet[x];
